1c98bec43750ac9c79c1f62bd4dbd9c1cdb647f1
Paolo Brunasti <paolo@brunasti.it>
2021-02-04 16:41:31 +0100

    Application parameters - check validity

--- main.go
+++ main.go
@@ -14,18 +14,36 @@ func main() {
 	   -r string
 	         Project remote repository URL
 	   -s    Project will have static assets or not
-
 	*/
 
 	projectLocation := flag.String("d", "", "Project location on disk")
 	projectName := flag.String("n", "", "Project name")
 	remoteRepositoryURL := flag.String("r", "", "Project remote repository URL")
 	staticAssets := flag.Bool("s", false, "Project will have static assets or not")
+
 	flag.Parse()
 
-	fmt.Println("scaffol-gen application")
-	fmt.Println(" - project name : ", *projectName)
-	fmt.Println(" - project location : ", *projectLocation)
-	fmt.Println(" - remote repository : ", *remoteRepositoryURL)
-	fmt.Println(" - static assets : ", *staticAssets)
+	flagError := false
+
+	if *projectName == "" {
+		fmt.Println("Project name cannot be empty")
+		flagError = true
+	}
+
+	if *projectLocation == "" {
+		fmt.Println("Project path cannot be empty")
+		flagError = true
+	}
+
+	if *remoteRepositoryURL == "" {
+		fmt.Println("Project repository URL cannot be empty")
+		flagError = true
+	}
+
+	if flagError {
+		return
+	}
+
+	fmt.Println("Generating scaffold for project", *projectName, "in", *projectLocation)
+	fmt.Println("  with repository", *remoteRepositoryURL, "and static assets", *staticAssets)
 }
