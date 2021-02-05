00150e754365771bdd2acaf62d2c14eeb92fcc62
Paolo Brunasti <paolo@brunasti.it>
2021-02-05 18:13:16 +0100

    Application Test - 02

--- main_test.go
+++ main_test.go
@@ -6,59 +6,45 @@ import (
 	"testing"
 )
 
-//func TestMain(m *testing.M) {
-//	// given
-//	output := strings.Builder{}
-//	log.SetOutput(&output)
-//	const name = "test-name"
-//	const path = "./test-path"
-//	var expectedOutput = fmt.Sprintf("Generating scaffold for project %s in %s", name, path)
-//	os.Args = []string{"scaffold_gen", "-n", name, "-d", path, "-r", "some-repo"}
+func TestExitOnError(t *testing.T) {
+	fmt.Println("TestExitOnError : =======================")
+	e := true
+	exitOnError(nil)
+	e = false
+	if e {
+		t.Errorf("exitOnError(nil) should have not killed the app")
+	} else {
+		t.Log("exitOnError(nil) Success")
+	}
+	fmt.Println("TestExitOnError : ================= - end")
+
+}
+
+//func TestExitOnErrorFail(t *testing.T) {
 //
-//	// when
-//	exitCode := m.Run()
+//	fmt.Println("TestExitOnErrorFail : =====================")
 //
-//	// then
-//	if exitCode != 0 { panic("exit code should have been 0") }
-//	if output.String() != expectedOutput { panic("output is not the expected one") }
+//	error := true
+//	err := errors.New("TEST")
+//	exitOnError(err)
+//	if error {
+//		t.Errorf("exitOnError(err) should have killed the app")
+//	}
+//
+//	fmt.Println("TestExitOnErrorFail : ================== end")
 //}
 
-func TestConfig_Validate(t *testing.T) {
+func TestTest(t *testing.T) {
+	errors := 3
+	fmt.Println("TestTest : ==================----")
 	t.Run(fmt.Sprintf("should report %d errors", 3), func(t *testing.T) {
-		errors := 1
-		if errors != 3 {
+		result := 2 + 1
+		if errors != result {
 			t.Fatalf("Expected %d errors, but got %d instead", 3, errors)
 		}
 	})
+	fmt.Println("TestTest : ================== end")
 }
-
-//func TestConfig_Validate(t *testing.T) {
-//	testCases := []struct {
-//		config projectStructureType
-//		numOfErrors int
-//	}{
-//		{ config: projectStructureType{}, numOfErrors: 3 },
-//		{ config: projectStructureType{Name: "name"}, numOfErrors: 2 },
-//		{ config: projectStructureType{Name: "name", Path: "path"}, numOfErrors: 1 },
-//		{ config: projectStructureType{Name: "name", Path: "path", RepositoryURL: "repository"}, numOfErrors: 0 },
-//	}
-//
-//	for _, testCase := range testCases {
-//		t.Run(fmt.Sprintf("should report %d errors", testCase.numOfErrors), func(t *testing.T) {
-//			// given
-//			config := testCase.config
-//
-//			// when
-//			errors := config.Validate()
-//
-//			// then
-//			if len(errors) != testCase.numOfErrors {
-//				t.Fatalf("Expected %d errors, but got %d instead", testCase.numOfErrors, len(errors))
-//			}
-//		})
-//	}
-//}
-//
 
 func TestMain(m *testing.M) {
 	// call flag.Parse() here if TestMain uses flags
